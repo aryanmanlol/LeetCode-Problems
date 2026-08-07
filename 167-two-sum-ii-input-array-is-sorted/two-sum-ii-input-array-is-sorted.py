6cdd4fbd2b1d07511778d51bafd065368be6c167
Aryanman <156987853+aryanmanlol@users.noreply.github.com>
2026-08-07 14:42:44 +0530

    Time: 4 ms (49.64%) | Memory: 20.6 MB (36.62%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -3,13 +3,14 @@ class Solution:
         left = 0
         right = len(numbers)-1
 
-        while left<right:
-            sum2 = numbers[left] + numbers[right]
-            if sum2==target:
-                return[left+1,right+1]
-            elif sum2<target:
+        for i in range(len(numbers)):
+            sol=numbers[left]+numbers[right]
+            if sol<target:
                 left+=1
-            else:
+            if sol>target:
                 right-=1
+            if sol==target:
+                return left+1,right+1
+
 
         
